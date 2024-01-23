d768a4afa56679e5606235a80c5a9403c785b76f
kkdaisy <daisy.kim231@gmail.com>
2024-01-23 13:52:27 +0900

    day30

---
+++ 05_JavaScript_Intro/app.js
@@ -0,0 +1,6 @@
+let greetingText = 'Hi, I\'m Daisy'; // let : 변수 생성. 세미콜론은 없어도 되지만 보기 편해서 씀
+let age = 32;
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = { title: 'Developer', place: 'New York', salary: 50000 };
+
+alert(hobbies[0]);
