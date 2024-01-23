89e2db00c755613e1b6f617058303172c281909c
kkdaisy <daisy.kim231@gmail.com>
2024-01-23 14:57:25 +0900

    day31

--- 05_JavaScript_Intro/app.js
+++ 05_JavaScript_Intro/app.js
@@ -1,6 +1,22 @@
-let greetingText = 'Hi, I\'m Daisy'; // let : 변수 생성. 세미콜론은 없어도 되지만 보기 편해서 씀
 let age = 32;
+let userName = `Daisy`;  // 백틱으로 띄어쓰기
 let hobbies = ['Sports', 'Cooking', 'Reading'];
-let job = { title: 'Developer', place: 'New York', salary: 50000 };
+let job = { 
+  title: 'Developer', 
+  place: 'New York', 
+  salary: 50000 
+};
 
-alert(hobbies[0]);
+//functions
+let totalAdultYears;
+
+function calculateAdultYears(userAge) {
+  return userAge - 18;
+}
+
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears);
+
+age = 45;
+totalAdultYears = calculateAdultYears(age);
+alert(totalAdultYears)
